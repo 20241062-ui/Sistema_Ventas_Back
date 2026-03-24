6c758a8f62fe57f622ee2a1407d095c963d5419a
20241062-ui <20241062@uthh.edu.mx>
2026-03-23 20:04:12 -0600

    si

--- src/controllers/analisisController.js
+++ src/controllers/analisisController.js
@@ -3,12 +3,12 @@ import { analisisModel } from "../models/analisisModel.js";
 export const getSimulacionStock = async (req, res) => {
     try {
         const producto = await analisisModel.obtenerDatosSimulacion('IPH15-001');
-        
+
         if (!producto) return res.status(404).json({ error: "Producto no encontrado" });
 
         const C = 15; // Stock inicial (t=0)
         const K = -0.223143; // Constante de decaimiento
-        
+
         // Fórmulas de la Memoria de Cálculo
         const tPedido = Math.log(10 / C) / K;      // Tiempo para 10 unidades
         const tAgotamiento = Math.log(1 / C) / K;  // Tiempo para 1 unidad (agotamiento)
