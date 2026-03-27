eb165d1070fd510afe778f06986afde99d23df8d
20241062-ui <20241062@uthh.edu.mx>
2026-03-26 20:58:38 -0600

    correccion en la ruta de analisisroutes

--- src/routes/analisisRoutes.js
+++ src/routes/analisisRoutes.js
@@ -2,6 +2,6 @@ import express from 'express';
 const router = express.Router();
 import { getSimulacionDinamica } from '../controllers/analisisController.js';
 
-router.get('/simulacion', getSimulacionDinamica);
+router.get('/simulacion/:noSerie', getSimulacionDinamica);
 
 export default router;
