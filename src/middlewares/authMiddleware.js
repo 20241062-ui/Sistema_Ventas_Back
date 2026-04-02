de85551676e1350457719aa888f10d960184f7d3
20241062-ui <20241062@uthh.edu.mx>
2026-04-02 12:32:47 -0600

    correccion en el middlware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,23 +3,20 @@ import jwt from 'jsonwebtoken';
 const SECRET_KEY = process.env.JWT_SECRET;
 
 export const verificarAdmin = (req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        return next();
+    }
     const authHeader = req.headers['authorization'];
-    
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({ message: 'Acceso denegado. Token con formato inválido o requerido.' });
     }
-
     const token = authHeader.split(' ')[1];
-
     try {
         const decoded = jwt.verify(token, SECRET_KEY);
-
         if (decoded.rol !== 'Administrador') {
             return res.status(403).json({ message: 'Acceso denegado. Se requieren permisos de administrador.' });
         }
-
         req.user = decoded; 
-        
         next(); 
     } catch (error) {
         return res.status(401).json({ message: 'Token inválido o expirado.' });
